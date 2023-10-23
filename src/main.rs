1f05a44a814d9de2f45d1066b586e7c1ac6941a6
nyzs <neazenmeh@gmail.com>
2023-10-23 12:35:45 +0200

    cors...

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder}
 use reqwest::header::USER_AGENT;
 use serde_json::Value;
 
-#[get("/{url}")]
+#[get("/")]
 async fn base() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
@@ -17,7 +17,6 @@ async fn catch_all(request: HttpRequest) -> impl Responder {
         .unwrap()
         .to_string();
 
-    // let old = reqwest::get(path).await.unwrap().text().await.unwrap();
     let data = client
         .get(path)
         .header(USER_AGENT, "Mythril / 0.1")
@@ -30,7 +29,6 @@ async fn catch_all(request: HttpRequest) -> impl Responder {
 
     let raw: Value = serde_json::from_str(&data).unwrap();
 
-    // HttpResponse::Ok().body()
     web::Json(raw)
 }
 
@@ -43,7 +41,7 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
-            .wrap(Cors::default())
+            .wrap(Cors::permissive())
             .service(base)
             .default_service(web::to(catch_all))
     })
